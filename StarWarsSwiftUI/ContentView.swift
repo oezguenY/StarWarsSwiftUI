1583eabfab2b034a4d6c957d2586e5a076aac98f
Ozgun Yildiz <ozgun.yildiz@hotmail.de>
2024-09-14 00:55:48 +0200

    Initial Commit

---
+++ StarWarsSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StarWarsSwiftUI
+//
+//  Created by Özgün Yildiz on 14.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StarWarsSwiftUI/StarWarsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsSwiftUIApp.swift
+//  StarWarsSwiftUI
+//
+//  Created by Özgün Yildiz on 14.09.24.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
